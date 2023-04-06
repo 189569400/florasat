4275a0e9571e067fdf32c9442c16e656e0253081
Robin Ohs <s8roohss@stud.uni-saarland.de>
2023-04-06 15:35:36 +0200

    Format: Fmt files + move into own namespace.

--- src/constellation/ConstellationCreator.h
+++ src/constellation/ConstellationCreator.h
@@ -8,56 +8,56 @@
 #ifndef CONSTELLATION_CONSTELLATIONCREATOR_H
 #define CONSTELLATION_CONSTELLATIONCREATOR_H
 
-#include <string.h>
 #include <omnetpp.h>
+#include <string.h>
+
 #include "mobility/NoradA.h"
 
-using namespace omnetpp;
+namespace flora {
+namespace constellation {
 
-namespace flora
-{
+class ConstellationCreator : public omnetpp::cSimpleModule {
+   public:
+    ConstellationCreator();
 
-    class ConstellationCreator : public cSimpleModule
-    {
-        public:
-            ConstellationCreator();
-        protected:
-            void CreateSatellites();
-            void CreateSatellite(int index, double raan, double meanAnomaly, int plane);
-            virtual void initialize() override;
-            // virtual void handleMessage(cMessage *msg) override;
+   protected:
+    void createSatellites();
+    void createSatellite(int index, double raan, double meanAnomaly, int plane);
+    virtual void initialize() override;
+    // virtual void handleMessage(cMessage *msg) override;
 
-        protected:
-        /** @brief Number of satellites in the constellation. */
-            int satCount;
+   protected:
+    /** @brief Number of satellites in the constellation. */
+    int satCount;
 
-            /** @brief Number of planes in the constellation. */
-            int planeCount;
+    /** @brief Number of planes in the constellation. */
+    int planeCount;
 
-            /** @brief Number of satellites in every plane. */
-            int satsPerPlane;
+    /** @brief Number of satellites in every plane. */
+    int satsPerPlane;
 
-            /** @brief Constellation inclination, given in deg. */
-            double inclination;
+    /** @brief Constellation inclination, given in deg. */
+    double inclination;
 
-            /** @brief Orbit height, given in km */
-            int altitude;
+    /** @brief Orbit height, given in km */
+    int altitude;
 
-            /** @brief The relative spacing between satellites in adjacent planes. */
-            int interPlaneSpacing;
+    /** @brief The relative spacing between satellites in adjacent planes. */
+    int interPlaneSpacing;
 
-            /** @brief The raan spread of the constellation. 180 for Walker Star, 360 for Walker Delta. */
-            int raanSpread;
+    /** @brief The raan spread of the constellation. 180 for Walker Star, 360 for Walker Delta. */
+    int raanSpread;
 
-            /** @brief Eccentricity of the constellation. */
-            double eccentricity;
+    /** @brief Eccentricity of the constellation. */
+    double eccentricity;
 
-            int baseYear;
-            double baseDay;
-            int epochYear;
-            double epochDay;
-    };
+    int baseYear;
+    double baseDay;
+    int epochYear;
+    double epochDay;
+};
 
-} // flora
+}  // namespace constellation
+}  // namespace flora
 
-#endif // CONSTELLATION_CONSTELLATIONCREATOR_H
+#endif  // CONSTELLATION_CONSTELLATIONCREATOR_H
